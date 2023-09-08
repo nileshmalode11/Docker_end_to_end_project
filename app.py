5bd252a1d512513acfbb0ef5156932da3eb75c69
Nilesh Malode <nileshmalode9829@gmail.com>
2023-09-08 19:12:31 +0530

    add changes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import json
 import pickle
-from flask import Flask,request,app ,jesonify,url_for,render_template,redirect_template,flash_template,session,escape
+
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
@@ -12,7 +14,6 @@ def home():
     return render_template('home.html') 
 
 @app.route("/predict_api", methods=["POST"])
-
 def predict_api():
     data=request.jeson["data"]
     print(data)
@@ -20,6 +21,13 @@ def predict_api():
     output = model.predict(data)
     print(output[0])
     return jesonify(output[0])
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(request.form.values())]
+    output = model.predict(data)
+    return render_template("home.html",predtiction_test="The House price is{}".format(output))
 
 if __name__=='__main__':
     app.run(debug=True)
